d3efc69dfb126f349c057574565590e8b1e029d2
Harrison <harrison@thebarkers.me.uk>
2017-08-29 18:43:29 +0100

    Inital MVC template

---
+++ aLevel/aLevel/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace aLevel
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
