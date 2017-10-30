b9d907ace7b0ca93645d12934aeee1565e6cc12c
Harrison <harrison@thebarkers.me.uk>
2017-10-30 22:07:08 +0000

    fixing

--- aLevel/aLevel/Controllers/HomeController.cs
+++ aLevel/aLevel/Controllers/HomeController.cs
@@ -31,26 +31,5 @@ namespace aLevel.Controllers
             return View("Index");
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-            
-            return View("About");
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Status()
-        {
-            ViewBag.Message = "Your Status page.";
-
-            return View();
-        }
-
     }
 }
